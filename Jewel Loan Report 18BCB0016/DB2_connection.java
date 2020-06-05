c976505ae12b4e5d749140826275fd113a821c02
conjurer2306 <44222378+conjurer2306@users.noreply.github.com>
2020-06-05 09:16:55 +0530

    Update DB2_connection.java

--- Jewel Loan Report 18BCB0016/DB2_connection.java
+++ Jewel Loan Report 18BCB0016/DB2_connection.java
@@ -8,73 +8,33 @@ public class DB2_connection
 {
   public static void main(String[] args) 
   {
-    	private String Customer_ID;
-	private String Name;
-	private int Loan_Amt;
-	private String Address;
-	private String contact;
-	private String DefaulterId;
+	  
+	  
+	  
+	  import java.sql.*;
 
-DB2_Connection obj_DB2_Connection=new DB2_Connection();
-		
-		System.out.println(obj_DB2_Connection.get_connection());
+public class Db_Connection 
+{
+	public static void main(String[] args)
+	{
+		Db2_Connection obj = new Db2_Connection();
+		System.out.println(obj.getConnection());
 	}
-	
-	
-	
-	public Connection get_connection(){
-		Connection connection=null;
-		try {
-			
-		
-		Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection("jdbc:mysql://localhost:6097/jewel_loan","root", "");
- 		
-		} catch (Exception e) {
-			System.out.println(e);
+	public Connection getConnection()
+	{
+		Connection connection = null;
+		//System.out.println("Connection failed");
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jewel_loan.report","root","hamartia");		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 		return connection;
-		}
-
-
-	public String getCustomer_Id() {
-		return Customer_Id;
-	}
-	public void setCustomer_Id(String Customer_Id) {
-		this.Customer_Id = Customer_Id;
-	}
-
-	public String getName() {
-		return Name;
-	}
-	public void setName(String Name) {
-		this.Name = Name;
-	}
-	public int getLoan_Amt() {
-		return Loan_Amt;
-	}
-	public void setLoan_Amt(int Loan_Amt) {
-		this.Loan_Amt = Loan_Amt;
-	}
-	public String getAddress() {
-		return Address;
-	}
-	public void setAddress(String Address) {
-		this.Address = Address;
-	}
-	public String getcontact() {
-		return contact;
-	}
-	public void setcontact(String contact) {
-		this.contact = contact;
-	}
-	public String getDefaulterId() {
-		return DefaulterId;
-	}
-	public void setDefaulterId(String DefaulterId) {
-		this.DefaulterId = DefaulterId;
 	}
 
 }
-}
-   
+	  
